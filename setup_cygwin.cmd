1d7274c959181bb74229ae905fc6eebd7dd963d8
Alexey Pakseykin <uvsmtid@gmail.com>
2013-09-17 06:13:26 +0000

    The first working version of the script.

--- setup_cygwin.cmd
+++ setup_cygwin.cmd
@@ -1,33 +1,35 @@
+@echo off
 REM This script provides automatic installation of Cygwin and its components
 REM for Interface with ACES (IACES).
 
+REM Hard-code distribution directory instead of making it depend on the 1st param
+REM DISTRIBDIR=%1
+set DISTRIBDIR=C:\cygwin.distrib
+
+set MIRRORDIR=http%%3a%%2f%%2fmirrors.kernel.org%%2fsourceware%%2fcygwin%%2f
+
+set ROOTDIR=C:\cygwin64
+
 REM Show hint for user.
-echo Specified distribution directory: %1
+echo Specified distribution directory: %DISTRIBDIR%
+
+echo Specified local packages directory: %DISTRIBDIR%\%MIRRORDIR%
 
 REM Run the setup providing list of all required components.
-%1\setup.exe --packages ^
+REM Note: the `^` character makes `cmd` interpreter concatenate lines.
+%DISTRIBDIR%\setup-x86_64.exe --packages ^
 unzip,^
 zip,^
 subversion,^
-autoconf,^
-automake,^
-libtool,^
-gcc4,^
-gcc4-g++,^
 gcc,^
 gcc-g++,^
 gdb,^
-libboost,^
 make,^
 libboost-devel,^
-libintl2,^
 vim,^
 openssh,^
-netcat,^
+nc,^
 cpio,^
-rpm,^
-rpm-build,^
-rpm-doc,^
 wget^
- -q -L -l %1\http%%3a%%2f%%2fmirrors.xmission.com%%2fcygwin%%2f
+ -q -L -l %DISTRIBDIR%\%MIRRORDIR% -R %ROOTDIR%
 
