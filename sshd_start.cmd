866f0c0b1538cecd3c121e0b4495ab1339209c29
Alexey Pakseykin <uvsmtid@gmail.com>
2013-09-25 14:07:57 +0000

    Fix change "" to '' inside outward ""

--- sshd_start.cmd
+++ sshd_start.cmd
@@ -10,7 +10,7 @@ REM Show IP configuration
 ipconfig
 
 REM Create key if it does not exists
-%ROOTDIR%\bin\bash.exe -c "if [ ! -e /home/sshd/ssh_host_dsa_key ] ; then /usr/bin/ssh-keygen -q -N "" -t dsa -f /home/sshd/ssh_host_dsa_key; fi"
+%ROOTDIR%\bin\bash.exe -c "if [ ! -e /home/sshd/ssh_host_dsa_key ] ; then /usr/bin/ssh-keygen -q -N '' -t dsa -f /home/sshd/ssh_host_dsa_key; fi"
 
 REM Run OpenSSH
 %ROOTDIR%\bin\bash.exe -c "/usr/sbin/sshd -f /home/sshd/sshd_config"
