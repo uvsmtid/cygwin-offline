5cc767735faef27c9840ebc32e8f141579af7b62
Alexey Pakseykin <uvsmtid@gmail.com>
2013-09-25 13:13:29 +0000

    Add initial version of file to setup OpenSSH

---
+++ setup_openssh.cmd
@@ -0,0 +1,20 @@
+@echo off
+REM This script sets up OpenSSH daemon for Cygwin
+
+set ROOTDIR=C:\cygwin64
+set DISTRIBDIR=C:\cygwin.distrib
+
+REM Show hint for user.
+echo Specified installation directory: %ROOTDIR%
+echo Specified distribution directory: %DISTRIBDIR%
+
+REM Create directory for OpenSSH daemon configuration
+mkdir %ROOTDIR%\home\sshd
+
+REM Copy OpenSSH daemon configuration file
+copy %DISTRIBDIR%\sshd_config       %ROOTDIR%\home\sshd
+
+REM Copy bat file to user's startup directory
+copy %DISTRIBDIR%\sshd_start.cmd    %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+
+
