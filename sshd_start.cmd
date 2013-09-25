a886725b30f0de8f245ca992d809031b5f962c39
Alexey Pakseykin <uvsmtid@gmail.com>
2013-09-25 13:51:23 +0000

    Add key creation (if doesn't exist) to ssh_start.cmd

--- sshd_start.cmd
+++ sshd_start.cmd
@@ -6,7 +6,12 @@ set ROOTDIR=C:\cygwin64
 REM Show hint for user.
 echo Specified installation directory: %ROOTDIR%
 
+REM Show IP configuration
 ipconfig
 
-%ROOTDIR%\bin\bash.exe -C "/usr/sbin/sshd -f /home/sshd/sshd_config"
+REM Create key if it does not exists
+%ROOTDIR%\bin\bash.exe -c "if [ ! -e /home/sshd/ssh_host_dsa_key ] ; then /usr/bin/ssh-keygen -q -N "" -t dsa -f /home/sshd/ssh_host_dsa_key; fi"
+
+REM Run OpenSSH
+%ROOTDIR%\bin\bash.exe -c "/usr/sbin/sshd -f /home/sshd/sshd_config"
 
