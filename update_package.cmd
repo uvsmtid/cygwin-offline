cec58b23e2c2db6fc93b1154becf30d4dc4c206e
Alexey Pakseykin <uvsmtid@gmail.com>
2013-09-26 21:48:56 +0000

    Add script to update components of Cygwin package automatically.

---
+++ update_package.cmd
@@ -0,0 +1,30 @@
+@echo off
+REM This updates Cygwin zip package.
+
+set CYGWINROOTDIR=C:\cygwin64
+set CYGWINDISTRIBDIR=C:\cygwin.distrib
+
+REM Show hint for user.
+echo Specified installation directory: %CYGWINROOTDIR%
+echo Specified distribution directory: %CYGWINDISTRIBDIR%
+
+REM TODO: Download base package automatically
+
+REM Download Cygwin update
+%CYGWINDISTRIBDIR%\repo\installer\download_cygwin.cmd
+
+REM Update each svn directory
+cd %CYGWINDISTRIBDIR%\repo
+svn update installer
+svn update sys
+
+REM Report
+pause "Press any key..."
+
+REM TODO: Archive package content
+
+
+REM TODO: Upload base package automatically
+
+
+
