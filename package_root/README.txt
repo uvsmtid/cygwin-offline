c9c2e109d66b018e6b6e11e02720e96d8f522848
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-07 09:43:19 +0000

    Add files from package root directory under version control

---
+++ package_root/README.txt
@@ -0,0 +1,3 @@
+See NEL wiki:
+http://nelwiki.asia.services.thales/index.php/Automatic_offline_Cygwin_installation
+
---
+++ package_root/install_cygwin.cmd
@@ -0,0 +1,4 @@
+REM This script is just a pointer to real installation script
+repo\installer\install_cygwin.cmd
+
+pause "press any key"
