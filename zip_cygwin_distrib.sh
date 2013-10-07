1175526923f85789de624a04312bf7120e6dea9f
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-07 11:31:16 +0000

    Save package outside of "cygwin.distrib" directory rather than Cygwin user's home.

--- zip_cygwin_distrib.sh
+++ zip_cygwin_distrib.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This script simply zips content of the entire
-# cygwin.distrib directory into archive under user's home.
+# cygwin.distrib directory saving package outside of it.
 
+set -e
 set -x
 
 UNIXCYGWINDISTRIBDIR="$(/usr/bin/cygpath -u "$CYGWINDISTRIBDIR")"
@@ -10,7 +11,7 @@ UNIXCYGWINROOTDIR="$(/usr/bin/cygpath -u "$CYGWINROOTDIR")"
 PACKAGE_NAME="$(/usr/bin/date '+%Y-%m-%d-T%H-%M-%S.%N').cygwin.distrib.zip"
 
 # Create timestamped archive containing "cygwin.distrib" in home directory
-/usr/bin/zip -r ~/"$PACKAGE_NAME" "$(/usr/bin/basename "$UNIXCYGWINDISTRIBDIR")"/ 1> /dev/null
+/usr/bin/zip -r "$UNIXCYGWINDISTRIBDIR/../$PACKAGE_NAME" "$(/usr/bin/basename "$UNIXCYGWINDISTRIBDIR")"/ 1> /dev/null
 
 # Communicate package name
 echo "$PACKAGE_NAME"
