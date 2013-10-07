8dff774f2e310206c55ba8dff73fdacd481dd6d5
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-07 11:44:27 +0000

    The first working version of update script for CI.

--- continuous_integration/create_cygwin_package_in_CI.cmd
+++ continuous_integration/create_cygwin_package_in_CI.cmd
@@ -1,26 +1,32 @@
 @echo on
-REM This updates Cygwin zip package (NEL updates only) inside continuous integration platform (CI).
+REM This script updates Cygwin zip package (NEL updates only) inside continuous integration platform (CI).
 REM It relies on installed Cygwin on the machine where it is called.
 REM It also relies on password-less (public key) authentication with server for uploading/downloading packages.
 
 REM Directory where this script is located
 set WORKING_DIR=%~dp0
 
+REM Server's IP address
+set NELARCHIVER=10.216.1.30
+
 REM SSH address
-SSH_ADDRESS=package@nelarchiver
+set SSH_ADDRESS=package@%NELARCHIVER%
 
 REM File server directory
 set REMOTE_DIR=nosync/cygwin
 
 REM Set paths to remote and local base backages
 set BASE_REMOTE_PACKAGE=%SSH_ADDRESS%:%REMOTE_DIR%/base.cygwin.distrib.zip
-set BASE_LOCAL_PACKAGE=base.zip
+set BASE_LOCAL_PACKAGE=base.cygwin.distrib.zip
 
 REM Use fullpath to bash of local Cygwin installation
-CYGWINROOTDIR=C:\cygwin64
+set CYGWINROOTDIR=C:\cygwin64
 
 REM Set Cygwin distributive directory
-set CYGWINDISTRIBDIR=%WORKING_DIRcygwin.distrib
+set CYGWINDISTRIBDIR=%WORKING_DIR%cygwin.distrib
+
+REM Remove existing (possibly residual) Cygwin distributive directory
+rmdir /s /q "%CYGWINDISTRIBDIR%"
 
 REM Download base package
 "%CYGWINROOTDIR%\bin\bash.exe" -c "cd \"$^(/usr/bin/cygpath -u \"$WORKING_DIR\"^)\" ; /usr/bin/scp \"$BASE_REMOTE_PACKAGE\" \"$BASE_LOCAL_PACKAGE\""
@@ -29,16 +35,29 @@ REM Unpack base package
 "%CYGWINROOTDIR%\bin\bash.exe" -c "cd \"$^(/usr/bin/cygpath -u \"$WORKING_DIR\"^)\" ; /usr/bin/unzip \"$BASE_LOCAL_PACKAGE\""
 
 REM Run offline updates inside the package
-"%CYGWINDISTRIBDIR%\repo\installer\update_cygwin_distribution_dir_repo_only.cmd"
+call "%CYGWINDISTRIBDIR%\repo\installer\update_cygwin_distribution_dir_repo_only.cmd"
 
 REM Pack updates and capture the single-line output with archive name
-for /f %%i in ("%CYGWINDISTRIBDIR%\repo\installer\zip_package.cmd") do set UPDATED_LOCAL_PACKAGE=%%i
+for /f %%i in ('"%CYGWINDISTRIBDIR%\repo\installer\zip_package.cmd"') do set UPDATED_LOCAL_PACKAGE=%%i
+echo %UPDATED_LOCAL_PACKAGE%
 
 REM Set remote path for updated package
-set UPDATED_REMOTE_PACKAGE=%SSH_ADDRESS%:%REMOTE_DIR%/%UPDATED_PACKAGE%
+set UPDATED_REMOTE_PACKAGE=%SSH_ADDRESS%:%REMOTE_DIR%/%UPDATED_LOCAL_PACKAGE%
+
+REM Switch to original directory (where the current script was called)
+cd "%WORKING_DIR%"
 
 REM Upload base package automatically and set its permissions to be readable
 "%CYGWINROOTDIR%\bin\bash.exe" -c "cd \"$^(/usr/bin/cygpath -u \"$WORKING_DIR\"^)\" ; /usr/bin/scp \"$UPDATED_LOCAL_PACKAGE\" \"$UPDATED_REMOTE_PACKAGE\""
+
+REM Switch to original directory (where the current script was called)
+cd "%WORKING_DIR%"
+
+
+REM Remove base package, updated package and Cygwin distributive directory
+rm "%BASE_LOCAL_PACKAGE%"
+rm "%UPDATED_LOCAL_PACKAGE%"
+rmdir /s /q "%CYGWINDISTRIBDIR%"
 
 
 
