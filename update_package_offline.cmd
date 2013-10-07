f1564e58c03f735483bcf50411ac9414688cba34
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-07 04:31:30 +0000

    Add script to automatically update Cygwin package offline

--- update_package_offline.cmd
+++ update_package_offline.cmd
@@ -12,27 +12,14 @@ REM Show hint for user.
 echo Specified installation directory: %CYGWINROOTDIR%
 echo Specified distribution directory: %CYGWINDISTRIBDIR%
 
-REM TODO: Download base package automatically
-
-REM Download Cygwin update
-%CYGWINDISTRIBDIR%\repo\installer\download_cygwin.cmd
-
 REM Update each svn directory
 cd %CYGWINDISTRIBDIR%\repo
 svn update installer
 svn update sys
 
 REM Archive all files by tar to preserve permissions (especially executable)
-%CYGWINROOTDIR%\bin\bash.exe -c "cd $UNIXCYGWINDISTRIBDIR ; tar -cvf repo.tar repo"
+%CYGWINROOTDIR%\bin\bash.exe -c "cd $UNIXCYGWINDISTRIBDIR ; /usr/bin/tar -cvf repo.tar repo"
 
-REM TODO: Archive package content
-
-
-REM TODO: Upload base package automatically
-
-
-REM Report
-pause "Press any key..."
-
-
+REM Archive package content into user's home:
+%CYGWINROOTDIR%\bin\bash.exe -c "$UNIXCYGWINDISTRIBDIR/repo/installer/zip_cygwin_distrib.sh"
 
