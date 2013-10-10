5ce14b559c7a76667cd3d22bc2b5b311ef68a3aa
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-10 13:57:35 +0000

    Fix: UNIXUNIXUSERPROFILE => UNIXUSERPROFILE

--- setup_openssh.sh
+++ setup_openssh.sh
@@ -16,5 +16,5 @@ mkdir -p "/home/sshd"
 cp -arf "$UNIXCYGWINDISTRIBDIR"/"repo/installer/sshd/sshd_config"        "/home/sshd"
 
 # Copy bat file to user's startup directory
-cp -arf "$UNIXCYGWINDISTRIBDIR"/"repo/installer/sshd/sshd_start.cmd"     "$UNIXUNIXUSERPROFILE"/"AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cp -arf "$UNIXCYGWINDISTRIBDIR"/"repo/installer/sshd/sshd_start.cmd"     "$UNIXUSERPROFILE"/"AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 
