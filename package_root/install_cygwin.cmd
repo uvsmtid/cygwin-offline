d6b32c5df6f3ccc68d23821c8b81cd5abf168488
Alexey Pakseykin <uvsmtid@gmail.com>
2013-10-10 13:38:09 +0000

    Switch back to original directory

--- package_root/install_cygwin.cmd
+++ package_root/install_cygwin.cmd
@@ -2,5 +2,14 @@ REM This script is just a pointer to real installation script
 call repo\installer\install_cygwin.cmd
 IF NOT %errorlevel%==0 (
     echo "Command returned: " %errorlevel%
+    cd "%~dp0"
     EXIT /B 1
 )
+REM Switch to original directory (where the current script was called)
+cd "%~dp0"
+IF NOT %errorlevel%==0 (
+    echo "Command returned: " %errorlevel%
+    EXIT /B 1
+)
+
+
