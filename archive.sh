1a6df8b32992d29fbf4d73acfeec384fee729b41
Alexey Pakseykin <uvsmtid@gmail.com>
2016-07-28 10:42:42 +0800

    Add tag to archive file

--- archive.sh
+++ archive.sh
@@ -6,12 +6,16 @@ set -u
 WRONG_DOWNLOAD_DIR_NAME="distrib/http%"
 CORRECT_DOWNLOAD_DIR_NAME="distrib/http%3a%2f%2fmirrors.kernel.org%2fsourceware%2fcygwin%2f"
 
+CURRENT_DIR_NAME="cygwin-offline.git"
+
+FILE_TAG="$(git describe --long)"
+
 # Fix directory name before archiving.
 mv "${WRONG_DOWNLOAD_DIR_NAME}" "${CORRECT_DOWNLOAD_DIR_NAME}"
 
 cd ..
 
-zip -r cygwin-offline.git.zip cygwin-offline.git
+zip -r "${CURRENT_DIR_NAME}.${FILE_TAG}.zip" "${CURRENT_DIR_NAME}"
 
 cd -
 
